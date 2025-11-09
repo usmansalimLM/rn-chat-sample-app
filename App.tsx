626ac78f7d3007200981f9c24e8c14042f75669d
Usman salim <usman.salim@likeminds.community>
2025-11-10 00:54:33 +0530

    integrated sdk

--- App.tsx
+++ App.tsx
@@ -1,118 +1,152 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
-import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
+  initMyClient,
+  Themes,
+} from "@likeminds.community/chat-rn-core";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { useState, useEffect } from "react";
 
+import React from "react";
+import { Platform } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+  CarouselScreen,
+  CreatePollScreen,
+  ImageCropScreen,
+  PollResult,
+  VideoPlayer,
+  ExploreFeed,
+  HomeFeed,
+  LMOverlayProvider,
+  ReportScreen,
+  ImageScreen,
+  ViewParticipants,
+  AddParticipants,
+  DmAllMembers,
+  SearchInChatroom,
+  CommunityChatScreen,
+  ScreenName,
+  FileUploadScreenWrapper,
+  ChatroomScreenWrapper,
+} from "@likeminds.community/chat-rn-core";
+import { ConversationState } from "@likeminds.community/chat-rn";
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
+const Stack = createStackNavigator();
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
+const App = () => {
+  const [myClient, setMyClient] = useState<any>();
+  const apiKey = "44c71864-19eb-4af9-beb7-f6de8a71944e";
+  const userName = "John Doe";
+  const uuid = "1234567890";
+  const theme = Themes.COMMUNITY_HYBRID
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+  useEffect(() => {
+    async function generateClient() {
+      const res: any = initMyClient([
+        ConversationState.MEMBER_LEFT_OPEN_CHATROOM,
+      ]);
+      setMyClient(res);
+    }
+    generateClient();
+  }, []);
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
+    <GestureHandlerRootView style={{flex: 1}}>
+      {userName && uuid && apiKey && myClient ? (
+        <LMOverlayProvider
+          myClient={myClient}
+          apiKey={apiKey}
+          userName={userName}
+          userUniqueId={uuid}
+          theme={theme}
+        >
+        <NavigationContainer>
+        <Stack.Navigator initialRouteName={ScreenName.CommunityChatScreen}>
+          <Stack.Screen
+            name={ScreenName.CommunityChatScreen}
+            component={CommunityChatScreen}
+          />
+          <Stack.Screen
+            name={ScreenName.SearchInChatroom}
+            component={SearchInChatroom}
+            options={{
+              gestureEnabled: Platform.OS === "ios" ? false : true,
+              headerShown: false,
+            }}
+          />
+          <Stack.Screen
+            name={ScreenName.ExploreFeed}
+            component={ExploreFeed}
+            initialParams={{
+              backIconPath: "",
+              filterIconPath: "",
+              participantsIconPath: "",
+              totalMessagesIconPath: "",
+              joinButtonPath: "",
+              joinedButtonPath: "",
+            }}
+          />
+          <Stack.Screen
+            name={ScreenName.Chatroom}
+            component={ChatroomScreenWrapper}
+            options={{
+              gestureEnabled: Platform.OS === "ios" ? false : true,
+            }}
+          />
+          <Stack.Screen
+            options={{
+              gestureEnabled: Platform.OS === "ios" ? false : true,
+            }}
+            name={ScreenName.FileUpload}
+            component={FileUploadScreenWrapper}
+            initialParams={{
+              backIconPath: "", // add your back icon path here
+              imageCropIcon: "", // add your image crop icon path here
+            }}
+          />
+          <Stack.Screen name={ScreenName.VideoPlayer} component={VideoPlayer} />
+          <Stack.Screen
+            options={{ gestureEnabled: false }}
+            name={ScreenName.CarouselScreen}
+            component={CarouselScreen}
+            initialParams={{
+              backIconPath: "", // add your back icon path here
+            }}
+          />
+          <Stack.Screen
+            options={{ gestureEnabled: false }}
+            name={ScreenName.PollResult}
+            component={PollResult}
+          />
+          <Stack.Screen
+            name={ScreenName.CreatePollScreen}
+            component={CreatePollScreen}
+          />
+          <Stack.Screen
+            options={{ headerShown: false }}
+            name={ScreenName.ImageCropScreen}
+            component={ImageCropScreen}
+          />
+          <Stack.Screen name={ScreenName.Report} component={ReportScreen} />
+          <Stack.Screen name={ScreenName.ImageScreen} component={ImageScreen} />
+          <Stack.Screen
+            name={ScreenName.ViewParticipants}
+            component={ViewParticipants}
+          />
+          <Stack.Screen
+            name={ScreenName.AddParticipants}
+            component={AddParticipants}
+          />
+          <Stack.Screen
+            name={ScreenName.DmAllMembers}
+            component={DmAllMembers}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+        </LMOverlayProvider>
+      ) : null}
+    </GestureHandlerRootView>
   );
-}
-
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-});
+};
 
 export default App;
